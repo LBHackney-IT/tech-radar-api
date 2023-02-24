36cf149e8fb25ca5d8fff47f16b8cc2206afa043
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2023-02-24 14:56:05 +0000

    creation of request object

---
+++ TechRadarApi/V1/Boundary/Request/PatchTechnologyItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TechRadarApi.V1.Boundary.Request
+{
+    public class PatchTechnologyItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Technique { get; set; }
+    }
+}
