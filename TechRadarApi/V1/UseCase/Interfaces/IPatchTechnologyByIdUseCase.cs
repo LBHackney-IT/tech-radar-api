bd9721249b30a6999774f4db335f2247d07c7afe
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2023-02-24 14:56:32 +0000

    patch use case and tests

--- TechRadarApi/V1/UseCase/Interfaces/IPatchTechnologyByIdUseCase.cs
+++ TechRadarApi/V1/UseCase/Interfaces/IPatchTechnologyByIdUseCase.cs
@@ -2,11 +2,12 @@ using TechRadarApi.V1.Boundary.Response;
 using System.Threading.Tasks;
 using System;
 using TechRadarApi.V1.Domain;
+using TechRadarApi.V1.Boundary.Request;
 
 namespace TechRadarApi.V1.UseCase.Interfaces
 {
     public interface IPatchTechnologyByIdUseCase
     {
-        Task<Technology> Execute(PatchByIdRequest pathParameters, UpdateTechnologyListItem bodyParameters);
+        Task<Technology> Execute(TechnologyResponseObject pathParameters, PatchTechnologyItem bodyParameters);
     }
 }
--- TechRadarApi/V1/UseCase/PatchTechnologyByIdUseCase.cs
+++ TechRadarApi/V1/UseCase/PatchTechnologyByIdUseCase.cs
@@ -5,14 +5,53 @@ using TechRadarApi.V1.UseCase.Interfaces;
 using System;
 using System.Threading.Tasks;
 using TechRadarApi.V1.Domain;
+using TechRadarApi.V1.Boundary.Request;
 
 namespace TechRadarApi.V1.UseCase
 {
     public class PatchTechnologyByIdUseCase : IPatchTechnologyByIdUseCase
     {
-        public async Task<Technology> Execute(PatchByIdRequest pathParameters, UpdateTechnologyListItem bodyParameters)
+        private ITechnologyGateway _gateway;
+        public PatchTechnologyByIdUseCase(ITechnologyGateway gateway)
         {
-            
+            _gateway = gateway;
+        }
+        public async Task<Technology> Execute(TechnologyResponseObject pathParameters, PatchTechnologyItem bodyParameters)
+        {
+            var technology = await _gateway.GetAll().ConfigureAwait(false);
+            if (technology == null)
+                return null;
+            if (pathParameters.Id != Guid.Empty)
+            {
+                var doesTechnologyExist = technology.Find(x => x.Id == pathParameters.Id);
+                if (doesTechnologyExist == null) return null;
+                technology.Remove(doesTechnologyExist);
+
+                var TechnologyData = new Technology()
+                {
+                    Id = (Guid) pathParameters.Id,
+                    Name = bodyParameters.Name ?? doesTechnologyExist.Name,
+                    Description = bodyParameters.Description ?? doesTechnologyExist.Description,
+                    Category = bodyParameters.Category ?? doesTechnologyExist.Category,
+                    Technique = bodyParameters.Technique ?? doesTechnologyExist.Technique
+                };
+
+                technology.Add(TechnologyData);
+            }
+            else
+            {
+                var TechnologyData = new Technology()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = bodyParameters.Name,
+                    Description = bodyParameters.Description,
+                    Category = bodyParameters.Category,
+                    Technique = bodyParameters.Technique
+                };
+                technology.Add(TechnologyData);
+            }
+            await _gateway.SaveTechRadar(technology).ConfigureAwait(false);
+            return technology;
         }
     }
 }
